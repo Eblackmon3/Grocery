8e4b437dfcdb0f22e8e5819b2543cc1d694b42ef
eblackmon3 <ericblackmon38@gmail.com>
2016-01-22 01:09:58 -0500

    Initial Commit including Parse intigration and user login check

---
+++ app/src/main/java/medellin/grocery/MainActivity.java
@@ -0,0 +1,47 @@
+package medellin.grocery;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String PARSE_APPLICATION_ID= "oWReLz33rUQNlHJz4pheHgnOdQM3NIqeYJ3ZNd6H";
+    private final String PARSE_CLIENT_KEY= "YIpvhkqCexkjErwlh8jyvJ7YtH1yLW8VDPdMRQ3D";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, PARSE_APPLICATION_ID, PARSE_CLIENT_KEY);
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+        //check if the user has logged in before
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isLogin = prefs.getBoolean("IS_LOGIN", false);
+
+        //Check if logged in, if they are send them to their homepage if not send them to registration page
+        // both pages TODO: 1/21/2016
+        if(isLogin){
+            //will containt the login activity
+
+        }else{
+            // Once the login activity has finished, set the IS_LOGIN prefernce to true so that the application knows not to relauch
+            // will use SharedPreferencesEditor
+            //Intent i = new Intent(this, loginActivity.class);
+            //startActivityForResult(i, 1);
+        }
+        setContentView(R.layout.activity_main);
+    }
+}
